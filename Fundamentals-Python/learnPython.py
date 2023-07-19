c276ed933eb891fe431de3414ec10ca41de8e6af
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-19 22:45:40 +0700

    Boolean Operators

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,7 +1,19 @@
-a = 1
-b = 2
+condition1 = True
+condition2 = False
 
-a == b  # false
-a != b  # true
-a > b  # false
-a < b  # true
+not condition1  # False
+condition1 and condition2  # False
+condition1 or condition2  # True
+
+0 or 1  # 1
+False or "hey"  # 'hey'
+'hi' or 'hey'  # 'hi'
+[] or False  # False
+False or []  # []
+
+0 and 1  # 0
+1 and 0  # 0
+False and "hey"  # False
+'hi' and 'hey'  # 'hey'
+[] and False  # []
+False and []  # False
