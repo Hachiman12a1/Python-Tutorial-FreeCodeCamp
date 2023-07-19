7e1b8b1be8c5050529076aeb463f68808d0db349
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-19 15:19:48 +0700

    f-strings

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def get_choices():
 
 
 choice = get_choices()
-print(choice)
 
 
 def check_win(player, computer):
-    print("You choose" + player + ", computer choose " + computer)
+    print(f"You choose {player} computer chose {computer}")
     if player == computer:
         return "It's a tie!"
+
+
+check_win("Player", "Ball")
