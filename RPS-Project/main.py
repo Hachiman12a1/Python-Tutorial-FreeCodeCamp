93f5bf060c57785d7708aa3bc5fb5589ce0a53b8
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-19 21:37:39 +0700

    Variables

---
+++ RPS-Project/main.py
@@ -0,0 +1,36 @@
+import random
+
+
+# def get_choices():
+#     player_choice = input("Enter a choice (rock, paper. scissors : ")
+#     options = ["rock", "paper", "scissors"]
+#     computer_choice = random.choice(options)
+#     choices = {"player": player_choice, "computer": computer_choice}
+#     return choices
+#
+#
+# choice = get_choices()
+
+
+def check_win(player, computer):
+    print(f"You choose {player} computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper Cover Rocks! You win!"
+        else:
+            return "Scissors cuts paper! You lose."
+    elif player == "Scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose!"
+
+
+print(check_win("Player", "Ball"))
