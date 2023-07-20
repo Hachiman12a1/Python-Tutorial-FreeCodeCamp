4a8feb6fc3a4ef12ef146ff066d7819b33f1c905
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 15:23:13 +0700

    Booleans

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,5 +1,13 @@
-name = "Dao Van Dat"
+done = False or ""
+print(done)
+print(type(done) == bool)
+print("yes") if done else print("no")
 
-print(name[2])
-print(name[1:5])
-print(name[2:])
+
+book_1_read = "hi"
+book_2_read = False
+
+read_any_book = any([book_1_read, book_2_read])
+read_all_book = all([book_1_read, book_2_read])
+print(read_any_book)
+print(read_all_book)
