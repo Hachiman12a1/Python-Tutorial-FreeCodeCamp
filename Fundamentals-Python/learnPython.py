821b8ce435059a8898a2a08502ec5897e5366247
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 22:38:19 +0700

    Dictionaries

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,12 +1,42 @@
-# Tuples
-names = ("Roger", "Syd", "Beau", "Dat")
+# Dictionaries
+dog = {"name": "Roger", "age": 8, "color": "black"}
 
-names[-1]
-names.index("Roger")
-len(names)
+dog['name'] = "Syd"
 
-print("Dat" in names)
-print(sorted(names))
+print(dog)
+print(dog.get('color', "green"))
 
-newTuple = names + ("Tina", "Quincy")
-print(newTuple)
+# Remove Item With key
+# dog.pop("age")
+# Remove last Item
+# dog.popitem()
+
+# Check Is Item In Dictionary
+print("color" in dog)
+
+# Keys of Dictionary
+print(dog.keys())
+print(list(dog.keys()))
+
+# Values of Dictionary
+print(dog.values())
+print(list(dog.values()))
+
+# Individual Items of Dictionary
+print(dog.items())
+print(list(dog.items()))
+
+# length of dictionary
+print(len(dog))
+
+# Add item to dictionary
+dog["favorite_food"] = "Meat"
+
+# Remove item from dictionary
+del dog["color"]
+print(dog)
+
+# Copy dictionary
+dogCopy = dog.copy()
+
+print(dogCopy)
