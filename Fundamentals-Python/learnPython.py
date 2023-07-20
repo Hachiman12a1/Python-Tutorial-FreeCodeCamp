dc0abb99f8f79a353bcab7ab2cbc1921449cf551
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 14:54:32 +0700

    String Characters Position & Slicing

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,18 +1,5 @@
-name = "Be\"au"
-name1 = "Be\'au"
-name2 = 'Be"au'
-name3 = 'Be"\'au'
-name4 = 'Be\nau'
-name5 = 'Be\au'
-name6 = "Be\\au"
+name = "Dao Van Dat"
 
-
-
-print(name)
-print(name1)
-print(name2)
-print(name3)
-print(name4)
-print(name5)
-print(name6)
-
+print(name[2])
+print(name[1:5])
+print(name[2:])
