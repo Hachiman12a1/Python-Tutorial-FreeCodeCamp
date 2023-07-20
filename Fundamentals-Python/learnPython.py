5aae29657d2eaacef39a93943de3282e6bafbdab
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 16:05:54 +0700

    User Input

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,14 +1,2 @@
-from enum import Enum
-
-
-class State(Enum):
-    INACTIVE = 0
-    ACTIVE = 1
-
-
-print(State.INACTIVE.value)
-print(State["INACTIVE"].value)
-print(State.INACTIVE)
-print(State["INACTIVE"])
-print(list(State))
-print(len(State))
+age = input("How old are you? : ")
+print(age)
