da5b0eaf01548037e0594c7d263913c369812532
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 14:06:06 +0700

    Ternary Operator

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,19 +1,2 @@
-condition1 = True
-condition2 = False
-
-not condition1  # False
-condition1 and condition2  # False
-condition1 or condition2  # True
-
-0 or 1  # 1
-False or "hey"  # 'hey'
-'hi' or 'hey'  # 'hi'
-[] or False  # False
-False or []  # []
-
-0 and 1  # 0
-1 and 0  # 0
-False and "hey"  # False
-'hi' and 'hey'  # 'hey'
-[] and False  # []
-False and []  # False
+def is_adult(age) :
+    return True if age >= 18 else False
