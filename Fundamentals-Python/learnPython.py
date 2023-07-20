02ae5cdf0da34e7d05aff275c3a38d1263e65b5c
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-20 14:42:08 +0700

    Escaping Characters

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,4 +1,18 @@
-print("bEAU".lower())
-print("bEAU".islower())
-print("bEAU people".title())
-print(len("bEAU"))
+name = "Be\"au"
+name1 = "Be\'au"
+name2 = 'Be"au'
+name3 = 'Be"\'au'
+name4 = 'Be\nau'
+name5 = 'Be\au'
+name6 = "Be\\au"
+
+
+
+print(name)
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(name5)
+print(name6)
+
