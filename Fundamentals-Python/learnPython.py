cc72ce74fea5c9c86bcb7e68680b31e3a87f2816
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 11:26:55 +0700

    Nested Functions

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,8 +1,25 @@
-# Variable Scope
-def test():
-    age = 8
-    print(age)
+# Nested Functions
+def count():
+    count = 0
+
+    def increment():
+        nonlocal count  # Sử dụng nonlocal keyword để tham chiếu biến count trong scope bên ngoài increment()
+        count += 1
+        print(count)
+
+    increment()
 
 
-print(age)  # Error Because of variable scope problem
-test()
+count()
+
+
+def talk(phrase) :
+    def say(word) :
+        print(word)
+        
+    words = phrase.split(" ")
+    for word in words:
+        say(word)
+        
+talk("My name is Dat")
+        
