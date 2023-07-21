c0403c0e81dc77d2385a47d14943ede85afff768
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 17:31:53 +0700

    Map, Filter, Reduce

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,6 +1,23 @@
-# Lambda Functions
-lambda number: number * 2
+#  Map, Filter, Reduce
+from functools import reduce
+numbers = [1, 2, 3, 4, 5, 6]
+expenses = [("Dinner" , 80), ("Breakfast" , 120)]
 
-multiply = lambda a,b: a * b
+def double(a):
+    return a*2
 
-print(multiply(2, 5))
+def filterDivision(a):
+    if(a %2 == 0):
+        return a
+    
+def expenseSum(a,b) :
+    return a[1] + b[1]
+
+mapResult = list(map(double, numbers))
+filterResult = list(filter(filterDivision, numbers))
+reduceResult = reduce(expenseSum, expenses)
+
+
+print(mapResult)
+print(filterResult)
+print(reduceResult)
