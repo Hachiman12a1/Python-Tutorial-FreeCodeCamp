5baa3e04013883580ede304b4119a2c53d78fb98
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 16:35:30 +0700

    Modules

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,22 +1,8 @@
-# Classes
-class Animal:
-    def __init__(self, name):
-        self.name = name
+from lib.dog import bark
+# Built-in libraries
+from math import sqrt
+    
+bark()
+print(int(sqrt(4)))
 
-    def speak(self):
-        print("The animal makes a sound.")
 
-class Dog(Animal):
-    def __init__(self, name, breed):
-        super().__init__(name)
-        self.breed = breed
-
-    def speak(self):
-        print("Woof!")
-
-# Tạo một đối tượng Dog và gọi phương thức speak
-my_dog = Dog("Max", "Labrador")
-my_dog.speak()  # In ra "Woof!"
-
-# Truy cập thuộc tính từ lớp cha
-print(my_dog.name)  # In ra "Max"  
