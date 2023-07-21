eb55e32e9b69211e836d48891d576c34ab978051
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 10:56:49 +0700

    Variable Scope

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,25 +1,8 @@
-# Functions
-def hello(name="My Friend", age=18):
-    print(f"Hello {name}, you are {str(age)} years old!")
+# Variable Scope
+def test():
+    age = 8
+    print(age)
 
 
-hello("dat", 23)
-
-
-def change(value):
-    value["name"] = "dat"
-
-
-val = {"name": "Beau"}
-change(val)
-
-print(val)
-
-
-def hello(name):
-    if not name:
-        return
-    print('Hello ' + name)
-
-
-hello("Dat")
+print(age)  # Error Because of variable scope problem
+test()
