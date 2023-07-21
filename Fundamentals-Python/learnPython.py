d771a5d45b12199572113d6449e4a77a8e5a9efe
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 21:17:12 +0700

    Recursion

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,23 +1,8 @@
-#  Map, Filter, Reduce
-from functools import reduce
-numbers = [1, 2, 3, 4, 5, 6]
-expenses = [("Dinner" , 80), ("Breakfast" , 120)]
+# Recursion
+def factorial(n) :
+    if n == 1: return 1
+    return n  * factorial(n - 1)
 
-def double(a):
-    return a*2
-
-def filterDivision(a):
-    if(a %2 == 0):
-        return a
-    
-def expenseSum(a,b) :
-    return a[1] + b[1]
-
-mapResult = list(map(double, numbers))
-filterResult = list(filter(filterDivision, numbers))
-reduceResult = reduce(expenseSum, expenses)
-
-
-print(mapResult)
-print(filterResult)
-print(reduceResult)
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
