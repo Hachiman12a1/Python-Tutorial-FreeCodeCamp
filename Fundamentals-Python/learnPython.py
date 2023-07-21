52eea20795f1c551bb519c59f283de0ebc9af5ed
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 21:59:17 +0700

    With

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,24 +1,9 @@
-# Exceptions
-try:
-    result = 2 /0
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-finally:
-    result = 1
+# With
+
+filename ='./text.txt'
+
+with open(filename, 'r') as file:
+    content = file.read()  
+    print(content)
     
-print(result)
-
-try:
-    raise Exception("An error!")
-except Exception as error:
-    print(error)
-
-
-class DogNotFoundException(Exception):
-    print("inside!")
-    pass
-
-try:
-    raise DogNotFoundException("An error!")
-except DogNotFoundException:
-    print("Dog not found!")
+    
