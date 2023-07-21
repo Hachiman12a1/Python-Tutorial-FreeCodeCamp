9cf22b752bc60b1193f283b7d4bea0dd690113bd
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 16:00:02 +0700

    Class And Inheritance

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -1,11 +1,22 @@
-# Break and continue
-items = ["a", "b", "c", "d", "e"]
-for item in items:
-    if(item == "a"):
-        continue
-    print(item)
-    
-for item in items:
-    if(item == "c"):
-        break
-    print(item)
+# Classes
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print("The animal makes a sound.")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def speak(self):
+        print("Woof!")
+
+# Tạo một đối tượng Dog và gọi phương thức speak
+my_dog = Dog("Max", "Labrador")
+my_dog.speak()  # In ra "Woof!"
+
+# Truy cập thuộc tính từ lớp cha
+print(my_dog.name)  # In ra "Max"  
