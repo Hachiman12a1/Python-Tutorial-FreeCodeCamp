17e5d44ca979839ece4478e2cad894f23c2ddf4c
Dao Van Dat <daovandat2k15@gmail.com>
2023-07-21 15:23:54 +0700

    Closure

--- Fundamentals-Python/learnPython.py
+++ Fundamentals-Python/learnPython.py
@@ -5,21 +5,16 @@ def count():
     def increment():
         nonlocal count  # Sử dụng nonlocal keyword để tham chiếu biến count trong scope bên ngoài increment()
         count += 1
-        print(count)
+        return count
 
-    increment()
-
-
-count()
+    return increment
 
 
-def talk(phrase) :
-    def say(word) :
-        print(word)
-        
-    words = phrase.split(" ")
-    for word in words:
-        say(word)
-        
-talk("My name is Dat")
+sum = count()
+
+print(sum())
+print(sum())
+print(sum())
+
+
         
